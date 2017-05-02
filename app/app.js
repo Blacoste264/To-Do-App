e4dde12e1fa9814aaffd483e430faf887bea0fee
Brittany Lacoste <brittanylacoste@Brittanys-MacBook-Air.local>
2017-05-02 12:34:50 -0700

    Added feature to add new todo

--- app/app.js
+++ app/app.js
@@ -1,27 +1,41 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app', []);
+  angular
+    .module('app', []);
 })();
 
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .controller('MainController', MainController);
+  angular
+    .module('app')
+    .controller('MainController', MainController);
 
-    MainController.$inject = [];
+  MainController.$inject = [];
 
-    /* @ngInject */
-    function MainController() {
-        var vm = this;
-        vm.title = "MainController";
-        activate();
+  /* @ngInject */
+  function MainController() {
+    var vm = this;
+    vm.title = "MainController";
+    vm.name = "Brittany";
+    //todo list
+    vm.todos = [{
+      taskName: "Write an Angular js Tutorial for Todo-List"
+    }];
 
-        function activate() {
+    //add new todo
+    vm.addTodo = function() {
+      vm.todos.push({
+        taskName: vm.newTodo,
+      });
+      vm.newTodo = ""; //Reset the text field.
+    };
 
-        }
+    activate();
+
+    function activate() {
+
     }
+  }
 })();
